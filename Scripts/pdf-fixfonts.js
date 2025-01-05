47f91d4379bf4e6bb0e0232258b868a620830672
Lei Zhao <LeiZh26@gmail.com>
2025-01-05 23:25:51 +0800

    Update pdf-fixfonts.js

--- Scripts/pdf-fixfonts.js
+++ Scripts/pdf-fixfonts.js
@@ -7,6 +7,14 @@ var mojibake = {
     '/#B7#BD#D5#FD#CA#E9#CB#CE_GBK': '方正书宋_GBK',
     '/#B7#BD#D5#FD#B4#F3#BA#DA_GBK': '方正大黑_GBK',
     '/#B7#BD#D5#FD#C1#A5#CA#E9_GBK': '方正隶书_GBK',
+    '/#B7#BD#D5#FD#D0#A1#B1#EA#CB#CE_GBK': '方正小标宋_GBK',
+    '/#B7#BD#D5#FD#B4#F3#B1#EA#CB#CE_GBK': '方正大标宋_GBK',
+    '/#C2#B7#C2#BD#C3#95#C3#BD#C2#B1#C2#A8#C3#8B#C3#8E_GBK': '方正报宋_GBK',
+    '/#C2#B7#C2#BD#C3#95#C3#BD#C2#BA#C3#9A#C3#8C#C3#A5_GBK': '方正黑体_GBK',
+    '/#C2#B7#C2#BD#C3#95#C3#BD#C3#90#C2#A1#C2#B1#C3#AA#C3#8B#C3#8E_GBK': '方正小标宋_GBK',
+    '/#C2#B7#C2#BD#C3#95#C3#BD#C2#B4#C3#B3#C2#B1#C3#AA#C3#8B#C3#8E_GBK': '方正大标宋_GBK',
+    '/#C2#B7#C2#BD#C3#95#C3#BD#C2#B4#C3#B3#C2#BA#C3#9A_GBK': '方正大黑_GBK',
+    '/#C2#B7#C2#BD#C3#95#C3#BD#C2#B7#C3#82#C3#8B#C3#8E_GBK': '方正仿宋_GBK',
     '/#B7#BD#D5#FD#CA#E9#CB#CE#BC#F2#CC#E5': '方正书宋简体',
     '/#B7#BD#D5#FD#BA#DA#CC#E5#BC#F2#CC#E5': '方正黑体简体',
     '/#B7#BD#D5#FD#BF#AC#CC#E5#BC#F2#CC#E5': '方正楷体简体',
@@ -14,11 +22,14 @@ var mojibake = {
     '/#B7#BD#D5#FD#B4#D6#D9#BB#B7#B1#CC#E5': '方正粗倩繁体',
     '/#BA#DA#CC#E5': '黑体',
     '/#CB#CE#CC#E5': '宋体',
+    '/#C3#8B#C3#8E#C3#8C#C3#A5': '宋体',
     '/#B2#D3#A9#FA#C5#E9': '細明體',
     '/#A5#FE#AFu#B7#A2#AE#D1': '全真楷書',
     '/#A5#FE#AFu#A4#A4#B6#C2#C5#E9': '全真中黑體',
     '/STSong-Light': 'STSong',
     '/STSong-Light,Bold': 'STZhongsong',
+    '/STSong-Light-UniGB-UCS2-H': 'STSong',
+    '/STSongStd-Light-Acro-UniGB-UTF16-H': 'STSong Std Acro Light',
 }
 
 var ascii = {
@@ -36,6 +47,8 @@ var psname = {
     '方正大黑_GBK': 'FZDHTK--GBK1-0',
     '方正隶书_GBK': 'FZLSK--GBK1-0',
     '方正粗倩繁体': 'FZCQFW--GB1-0',
+    '方正小标宋_GBK': 'FZXBSK--GBK1-0',
+    '方正大标宋_GBK': 'FZDBSK--GBK1-0',
     '宋体': 'SimSun',
     '黑体': 'SimHei',
     '細明體': 'MingLiU',
@@ -83,8 +96,7 @@ function fixFamily(fd) {
 }
 
 function fixFont(val, key) {
-    if (val.Subtype.asName() === 'Type0'
-        && val.DescendantFonts[0].Subtype.asName() === 'CIDFontType2') {
+    if (val.Subtype.asName() === 'Type0') {
         var fd = val.DescendantFonts[0].FontDescriptor;
         fixEncoding(val, 'BaseFont', mojibake, 'toString');
         fixEncoding(val.DescendantFonts[0], 'BaseFont', mojibake, 'toString');
@@ -118,9 +130,10 @@ function findFont(obj, func) {
         if (typeof obj.Resources !== 'undefined'
             && typeof obj.Resources.Font !== 'undefined')
             obj.Resources.Font.forEach(func);
-        else if (obj.Type.asName() !== 'Page'
-                 && (obj.Type.asName() !== 'XObject'
-                     || obj.Subtype.asName() !== 'Form'))
+        if (typeof obj.Type === 'undefined'
+            || obj.Type.asName() !== 'Page'
+            && (obj.Type.asName() !== 'XObject'
+                || obj.Subtype.asName() !== 'Form'))
             ;
         else if (typeof obj.Resources.XObject !== 'undefined')
             obj.Resources.XObject.forEach(findFontExec);
